59583136ecf8348cf2642a85a5c36edc92758466
Mimi Ke <mimi.g.ke@gmail.com>
2021-04-29 10:24:42 -0500

    thursday morn

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -35,7 +35,7 @@ const LoginForm = () => {
   return (
     <>
       <div className="log-in-container">
-        <div className="splash-image">
+        <div className="phones-image">
           <img alt="cell phone" src="/images/splash.jpeg"></img>
         </div>
 
@@ -43,42 +43,48 @@ const LoginForm = () => {
           <div className="icon">
                 <h1>Instagram</h1>
           </div>
-        <form onSubmit={onLogin}>
-        <div>
-          {errors.map((error) => (
-            <div>{error}</div>
-          ))}
-        </div>
-        <div>
-          <input
-            name="email"
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={updateEmail}
-          />
-        </div>
-        <div>
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={updatePassword}
-          />
-          <div className="log-in">
-            <button type="submit">Login</button>
-          </div>
-
+        <div className="log-in-form">
+          <form onSubmit={onLogin}>
           <div>
-            <h5 style={{color:'grey'}}>---------------------   OR  ---------------------</h5>
+            {errors.map((error) => (
+              <div>{error}</div>
+            ))}
           </div>
+          <div>
+            <input
+              name="email"
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div>
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={updatePassword}
+            />
+            <div className="log-in">
+              <button type="submit">Login</button>
+            </div>
+          </div>
+        </form>
+      
+      <span class="or-divider">OR</span>
 
-          <div className="facebook-link">
-              <a href="https://www.facebook.com"  style={{textDecoration:'none', fontSize:"1.1em", color:"#4267B2"}}>Log in with Facebook<i class="fab fa-facebook" style={{fontSize: '1.5em'}}></i></a>
-              </div>
+        <div className="facebook-login">
+              <a href="https://www.facebook.com"  style={{textDecoration:'none', fontSize:"1.1em", color:"#4267B2"}}><i class="fab fa-facebook-square" area-hidden="true" style={{fontSize: '1.5em'}}></i></a>
+              <a href="https://www.facebook.com"  style={{textDecoration:'none', fontSize:"1.1em", color:"#4267B2"}}>  Log in with Facebook</a>
         </div>
-      </form>
+        <div className="forgot">
+          <a href="/">Forgot password?</a>
+        </div>
+        </div>
+
+
         <div className="sign-up-box">
             <h3>
               Don't have an account? <Link to="/sign-up">Sign up</Link>
@@ -88,10 +94,8 @@ const LoginForm = () => {
 
       </div>
 
-
-
-      <div className="get-app-text">
-          <h4>Get the app.</h4>
+      <div className="get-app">
+          <p>Get the app.</p>
       </div>
 
       <div className="app-icon-container">
@@ -102,6 +106,20 @@ const LoginForm = () => {
           <img alt="apple app" src="/images/apple-app.png"></img>
         </span>
       </div>
+
+      <footer>
+        <ul class="footer-links">
+          <li><a href="/">About</a></li>
+          <li><a href="/">Blog</a></li>
+          <li><a href="/">Jobs</a></li>
+          <li><a href="/">API</a></li>
+          <li><a href="/">Privacy</a></li>
+          <li><a href="/">Terms</a></li>
+          <li><a href="/">Top Accounts</a></li>
+          <li><a href="/">Hashtags</a></li>
+          <li><a href="/">Locations</a></li>
+        </ul>
+      </footer>
 
     </>
 
