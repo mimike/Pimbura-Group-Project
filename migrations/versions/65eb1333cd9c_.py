4761fe60f4e986341f3a78121a0d1a47907f527c
Mimi Ke <mimi.g.ke@gmail.com>
2021-04-30 17:07:10 -0500

    resize

--- migrations/versions/65eb1333cd9c_.py
+++ migrations/versions/65eb1333cd9c_.py
@@ -1,8 +1,8 @@
-"""new tables
+"""empty message
 
-Revision ID: 723d1a6b386e
+Revision ID: 65eb1333cd9c
 Revises: 
-Create Date: 2021-04-30 10:42:17.670943
+Create Date: 2021-04-30 16:58:14.037342
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '723d1a6b386e'
+revision = '65eb1333cd9c'
 down_revision = None
 branch_labels = None
 depends_on = None
