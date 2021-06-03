bd226796618e2df2afbb7c94d2abcf1a0e9f1f88
Heba <hkasaby83@gmail.com>
2021-06-03 11:47:04 -0500

    Working on the modal1

--- react-app/src/components/ExplorePage/index.js
+++ react-app/src/components/ExplorePage/index.js
@@ -63,7 +63,7 @@ const ExplorePage = () => {
                             {/* {console.log("++++++++++++++",Object.values(post))} */}
                                 <button className="post-image-button" onClick={onClick} value={post.id}>
                                     <div className="explore-tile">
-                                        <img  src={post.photo_url} value={post}/>
+                                        <img  className="explore-tile" src={post.photo_url} value={post}/>
                                     </div>
                                     {/* <img  src={post.photo_url} value={post}/> */}
                                 </button>
@@ -76,50 +76,50 @@ const ExplorePage = () => {
                     <div className="modal">
                         <div className="post-image-div"><img className="post-image" src={targetedPhoto} /></div>
                         <div className="post-content">
-                            <NavLink to={`/user/${userProfile}`}>
-                                <div className="first">
-                                    <div><img className="user-avatar" src={postUser.avatar_url}></img></div>
+                            <div className="first">
+                                <NavLink to={`/user/${userProfile}`} className="first-nav">
+                                    <div className="first-img"><img className="user-avatar" src={postUser.avatar_url}></img></div>
                                     <div className="user-follow">
-                                        <div className="username-bold">{postUser.username}<div className="follow"><NavLink to="/follow">Follow</NavLink></div></div>
-                                    {/* <div className="username-light">{postUser.username}</div> */}
+                                        <div className="username-bold">{postUser.username}</div>
+                                        <div className="follow"><NavLink to="/follow">Follow</NavLink></div>
+                                        {/* <div className="username-light">{postUser.username}</div> */}
                                     </div>
-                                </div>
-                            </NavLink>
+                                </NavLink>
+                            </div>
                             <NavLink to={`/user/${userProfile}`}>
                                 <div className="user-avatar-div2">
                                     <div><img className="user-avatar" src={postUser.avatar_url}></img></div>
                                     <div className="username-bold2">{postUser.username} </div>
                                 </div>
                             </NavLink>
-                                <div className="post-caption">{targetimg.caption}</div>
-                                <div className="comments-div">
-                                    <ul className="comments"> {comments.map((comment,i) =>
-                                        <li className="comments-list" key={i}>
-                                            {/* {console.log("++++++++++++++",comment)} */}
-                                        <NavLink to={`/user/${comment.user_id}`}>
-                                            <div className="user-comment-avatar-div">
-                                                <div >
-                                                    <img className="user-comment-avatar" src={comment.user.avatar_url}/>
-                                                    <div className="user-comment">{comment.user.username}</div>
-                                                </div>
+                            <div className="post-caption">{targetimg.caption}</div>
+                            <div className="comments-div">
+                                <ul className="comments"> {comments.map((comment,i) =>
+                                    <li className="comments-list" key={i}>
+                                        {/* {console.log("++++++++++++++",comment)} */}
+                                    <NavLink to={`/user/${comment.user_id}`}>
+                                        <div className="user-comment-avatar-div">
+                                            <div >
+                                                <img className="user-comment-avatar" src={comment.user.avatar_url}/>
+                                                <div className="user-comment">{comment.user.username}</div>
                                             </div>
-                                        </NavLink>
-                                            <div className="comment">
-                                                {comment.comment}
-                                             </div>
-                                        </li>
-                                        )}
-                                    </ul>
-                                </div>
-                                <div className='icons'>
-
-                                    <i className="heart outline icon"></i>
-                                    <i className="comment outline icon"></i>
-                                    <i className="paper plane outline icon"></i>
-                                </div>
-                                <div className="post-comment-div">
-                                    <Comments  className="post-comment" post_id={post_id}/>
-                                </div>
+                                        </div>
+                                    </NavLink>
+                                        <div className="comment">
+                                            {comment.comment}
+                                            </div>
+                                    </li>
+                                    )}
+                                </ul>
+                            </div>
+                            <div className='icons'>
+                                <i className="heart outline icon"></i>
+                                <i className="comment outline icon"></i>
+                                <i className="paper plane outline icon"></i>
+                            </div>
+                            <div className="post-comment-div">
+                                <Comments  className="post-comment" post_id={post_id}/>
+                            </div>
                         </div>
                     </div>
                 </Modal>
